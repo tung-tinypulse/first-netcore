ddae857211681de1b10c26442f6b6c6565add9ab
Tung Tram <tungtram@vinasource.com>
2018-05-16 15:38:50 +0700

    first commit

---
+++ app/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using library;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"The answer is {new Thing().Get(19, 23)}");
+        }
+    }
+}
---
+++ library/Thing.cs
@@ -0,0 +1,10 @@
+﻿using static Newtonsoft.Json.JsonConvert;
+
+namespace library
+{
+    public class Thing
+    {
+        public int Get(int left, int right) =>
+            DeserializeObject<int>($"{left + right}");
+    }
+}
